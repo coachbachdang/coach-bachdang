9b8a38e026403eba10c4b0653eac595154482e32
Hoang Bach Dang <hoangbachdang@gmail.com>
2026-07-07 01:38:55 +0700

    Update Apps Script URL to latest deployment

--- api/submit.js
+++ api/submit.js
@@ -1,4 +1,4 @@
-﻿const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzfLy7__P0y-Jy96E-CLlUQYc3CpIoMcbEiZQ1H1OMxUEo06dLN1lTyeT3J04gfcPsukw/exec';
+﻿const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbymRkZ6Mh8MMrc4kigoNNZAxLW-m3BXQPar42oRvKf6Z4mBlKkDzCCCjJ4gl0wynlgqPQ/exec';
 
 module.exports = async (req, res) => {
   if (req.method !== 'POST') return res.status(405).end();
